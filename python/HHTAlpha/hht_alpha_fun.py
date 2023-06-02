4478b1d8ea60da44db304aa8a03a454e79a3900c
Justin Porter <jp88@rice.edu>
2023-06-02 14:18:35 -0600

    HHT alpha functions and unit test to be compared to nalu-wind

--- python/HHTAlpha/hht_alpha_fun.py
+++ python/HHTAlpha/hht_alpha_fun.py
@@ -5,7 +5,20 @@ Functions for running HHT Alpha algorithm
 import numpy as np
 
 
-def hht_alpha_update(xn, vn, an, Mmat, Cmat, Kmat, alpha, tn, dt, Fextfun):
+def hht_alpha_predict(xn, vn, an, xnm1, vnm1, anm1, dt):
+    """ 
+    Predict states to pass to aero model
+    """
+
+    xpred_np1 = xn + dt*(1.5*vn - 0.5*vnm1);   
+
+    vpred_np1 = vn + dt*(1.5*an - 0.5*anm1);  
+
+    return xpred_np1, vpred_np1
+
+
+def hht_alpha_update(xn, vn, an, Mmat, Cmat, Kmat, alpha, tn, dt, Fn, Fnp1,
+                     load_scale=1.0, T=np.eye(3)):
     """
     Advance one step with the HHT-Alpha method
     
@@ -38,7 +51,9 @@ def hht_alpha_update(xn, vn, an, Mmat, Cmat, Kmat, alpha, tn, dt, Fextfun):
     
     left_mat = Mmat + ((1+alpha)*dt*gamma)*Cmat + ((1+alpha)*(dt**2)*beta)*Kmat
 
-    Fext = Fextfun(tn + (1+alpha)*dt)
+    Fext = Fnp1*(1 + alpha) - alpha * Fn
+
+    Fext = T @ (load_scale * Fext)
 
     right_vec = -Cmat @ (vn + (1+alpha)*dt*(1-gamma)*an) \
                 -Kmat @ (xn + (1+alpha)*dt*vn + (1+alpha)*(dt**2)/2*(1-2*beta)*an) \
@@ -55,7 +70,7 @@ def hht_alpha_update(xn, vn, an, Mmat, Cmat, Kmat, alpha, tn, dt, Fextfun):
     return xnp1, vnp1, anp1
 
 
-def hht_alpha_integrate(x0, v0, Mmat, Cmat, Kmat, alpha, dt, Fextfun, t0, t1):
+def hht_alpha_integrate(x0, v0, Mmat, Cmat, Kmat, alpha, dt, Fextfun, t0, t1, load_scale=1.0, T=np.eye(3)):
     """
     Run HHT-Alpha integration problem
     
@@ -90,7 +105,7 @@ def hht_alpha_integrate(x0, v0, Mmat, Cmat, Kmat, alpha, dt, Fextfun, t0, t1):
 
     # Calculate Initial Acceleration
     #   M a + C v + K x = Fext
-    right_vec_0 = Fextfun(t0) - Cmat @ v0 - Kmat @ x0
+    right_vec_0 = Fextfun(t0, x0, v0) - Cmat @ v0 - Kmat @ x0
 
     a0 = np.linalg.solve(Mmat, right_vec_0)
 
@@ -99,19 +114,38 @@ def hht_alpha_integrate(x0, v0, Mmat, Cmat, Kmat, alpha, dt, Fextfun, t0, t1):
     vhist[:, 0] = v0
     ahist[:, 0] = a0
 
+    # Time n-1 states for predictor
+    xnm1 = np.zeros_like(x0)
+    vnm1 = np.zeros_like(x0)
+    anm1 = np.zeros_like(x0)
+
     thist = t0 + dt*np.array(range(time_steps+1))
 
 
     # Run time integration
     for tind in range(time_steps):
 
-        xnp1, vnp1, anp1 = hht_alpha_update(xhist[:, tind], vhist[:, tind], ahist[:, tind], \
-                                            Mmat, Cmat, Kmat, \
-                                            alpha, t0+dt*tind, dt, Fextfun)
-        
+        # Predict
+        xpred_np1,vpred_np1 = hht_alpha_predict(xhist[:, tind], vhist[:, tind], ahist[:, tind], xnm1, vnm1, anm1, dt)
+
+        # Evaluate Forces
+        Fn = Fextfun(t0+dt*tind, xhist[:, tind], vhist[:, tind])
+ 
+        Fnp1 = Fextfun(t0+dt*(tind+1), xpred_np1, vpred_np1)
+
+        # Update step
+        xnp1, vnp1, anp1 = hht_alpha_update(xhist[:, tind], vhist[:, tind], ahist[:, tind], 
+                                           Mmat, Cmat, Kmat, alpha, 
+                                           t0+dt*tind, dt, Fn, Fnp1,
+                                           load_scale=load_scale, T=T)
+
         xhist[:, tind+1] = xnp1
         vhist[:, tind+1] = vnp1
         ahist[:, tind+1] = anp1
+
+        xnm1 = xhist[:, tind]
+        vnm1 = vhist[:, tind]
+        anm1 = ahist[:, tind]
 
     return thist, xhist, vhist, ahist
 
---
+++ python/HHTAlpha/unit_tests.py
@@ -0,0 +1,98 @@
+"""
+Python solves of the unit tests for the Airfoil SMD
+"""
+
+import numpy as np
+from scipy.linalg import eigh
+
+import hht_alpha_fun as hhta
+
+######
+# Approximate matrices (rounded) from a 3 DOF model
+
+M = np.array([7000.0, -90.0, 60.0, -90.0, 6400.0, 1800.0, 60.0, 1800.0, 7800]).reshape(3,3)
+
+K = np.array([ 131000.0, 7700.0, -97000.0, 7700.0, 66000.0, -31000.0, -97000.0, -31000.0, 4800000.0]).reshape(3,3)
+C = np.array([300.0, 8.0, -70.0, 8.0, 200.0, 20.0, -70.0, 20.0, 3700.0]).reshape(3,3)
+
+T = np.array([62.0, 0.0, 10.0, 0.0, 61.0, 0.0,  1.0, -6.0, 60.0]).reshape(3,3)
+
+Tident = np.eye(3)
+
+######
+# Verify rounded matrices still make sense
+
+eigvals,eigvecs = eigh(K, M)
+
+print(eigvals)
+
+print(eigvecs.T @ C @ eigvecs)
+
+
+#####
+# Do an update
+
+xn = np.array([0.1, 0.2, 0.01])
+vn = np.array([0.0, 0.0, 0.0])
+an = np.array([0.0, 0.0, 0.0])
+Fn = np.array([0.0, 0.0, 0.0])
+Fnp1 = np.array([250.0, 350.0, 500.0])
+
+load_scale = 0.75
+
+tn = 0.0
+dt = 0.01
+alpha = -0.05
+
+xnp1, vnp1, anp1 = hhta.hht_alpha_update(xn, vn, an, M, C, K, alpha, tn, dt, 
+                      Fn, Fnp1, load_scale=load_scale, T=Tident)
+print('\nUpdate step with identity Tmap')
+print('X, V, A')
+print(str(xnp1.tolist()))
+print(str(vnp1.tolist()))
+print(str(anp1.tolist()))
+
+#####
+# Do an update with the force mapping
+
+xn = np.array([0.1, 0.2, 0.01])
+vn = np.array([0.2, 0.3, -0.05]) # Add a nonzero value 
+an = np.array([0.3, 0.2, 0.0]) # Add a nonzero value
+Fn = np.array([200.0, 400.0, 50.0]) # Add a nonzero value
+Fnp1 = np.array([250.0, 350.0, 500.0])
+
+load_scale = 0.75
+
+tn = 0.0
+dt = 0.01
+alpha = -0.05
+
+xnp1, vnp1, anp1 = hhta.hht_alpha_update(xn, vn, an, M, C, K, alpha, tn, dt, 
+                      Fn, Fnp1, load_scale=load_scale, T=T)
+print('\nUpdate step with nonidentity Tmap')
+print('X, V, A')
+print(str(xnp1.tolist()))
+print(str(vnp1.tolist()))
+print(str(anp1.tolist()))
+
+#####
+# Do a predict step
+
+xn = np.array([0.1, 0.2, 0.01])
+vn = np.array([1.0, -2.0, 0.05])
+an = np.array([-0.5, -7.0, 1.0])
+
+xnm1 = np.array([0.0, 0.0, 0.0]) # doesn't matter for predictor
+vnm1 = np.array([1.1, -1.4, -0.05])
+anm1 = np.array([0.5, -8.0, 2.0])
+
+dt = 0.01
+
+xpred_np1, vpred_np1 = hhta.hht_alpha_predict(xn, vn, an, xnm1, vnm1, anm1, dt)
+
+
+print('\nPredict step')
+print('X, V')
+print(str(xpred_np1.tolist()))
+print(str(vpred_np1.tolist()))
+
