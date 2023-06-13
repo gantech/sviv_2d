6cdfcbcfca2fba08352116fce8ed167e82df29ec
Justin Porter <jp88@rice.edu>
2023-06-13 16:05:58 -0600

    Updated all of the modal calculations and now gives most of the model details

--- python/SimpleBeam/Analytical/beam_modes.py
+++ python/SimpleBeam/Analytical/beam_modes.py
@@ -2,77 +2,145 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def unscaled_modes(x, L, beta):
+def mass_norm_modes(x, L, beta, rho):
     """
-    Mode shapes without mass normalization
+    Beam Modes that are mass normalized
+    Cantilever beam with x=0 fixed
+
+    Inputs:
+      x - locations to evaluate
+      L - length of beam
+      beta - parameter of beam modes (depends on mode number)
+      rho - mass per unit length of the beam
     """
 
-    
-    val = (np.sin(beta*L) - np.sinh(beta*L))*(np.sin(beta*x) - np.sinh(beta*x)) \
-          + (np.cos(beta*L) - np.cosh(beta*L))*(np.cos(beta*x) - np.cosh(beta*x)) 
+    val = 1.0 / np.sqrt(rho * L) * ( np.cos(beta * x) - np.cosh(beta*x) \
+            + (np.sin(beta*L) - np.sinh(beta*L)) / (np.cos(beta*L) + np.cosh(beta*L)) \
+            * (np.sin(beta*x) - np.sinh(beta*x)) )
 
     return val
 
-def mass_norm_val(L, beta, rho):
+def unif_modal_loading(L, beta, rho):
     """
-    Calculate the mass normalization constant for the mode shape
+    Modal loading for a constant load distribution
     """
 
-    # 1 = A^2*rho * inv_Asqrho for mass normalization
-    inv_Asqrho = (0.25/beta) * ( np.sin(beta*L) * np.sinh(beta*L) )**2 * ( -np.sin(2*beta*L) + np.sinh(2*beta*L) \
-         + 4*np.cos(beta*L)*np.sinh(beta*L) - 4*np.sin(beta*L)*np.cosh(beta*L) ) \
-         + (1.0/beta) * (np.sin(beta*L) - np.sinh(beta*L) )**3 * (np.cos(beta*L) - np.cosh(beta*L)) \
-         + (0.25/beta) * (np.cos(beta*L)*np.cosh(beta*L))**2 * (4.0*beta*L + np.sin(2*beta*L) \
-         + np.sinh(2*beta*L) - 4.0*np.cos(beta*L)*np.sinh(beta*L) - 4.0*np.sin(beta*L)*np.cosh(beta*L) )
+    val = -2.0 * (np.cosh(beta*L) - 1.0)*(np.sin(beta*L) - np.sinh(beta*L)) \
+           / np.sqrt(rho*L) / beta / (np.cos(beta*L) - np.cosh(beta*L))
 
-    print(inv_Asqrho)
+    return val 
 
-    Asq = 1.0 / inv_Asqrho / rho
+def construct_analytical_model(L, beta, rho, rotJ, span_frac, stiffness_vec, betaL):
+    """
+    Construct a 3 DOF model using the analytical beam modes etc.
+    """
 
-    return np.sqrt(Asq)
+    bending_phi = mass_norm_modes(span_frac*L, L, beta, rho)
+    torsion_phi = mass_norm_modes(span_frac*L, L, beta, rotJ)
 
-def mass_norm_modes(x, L, beta, rho):
+    Phi = np.diag([bending_phi, bending_phi, torsion_phi])
 
-     unscaled_val = unscaled_modes(x, L, beta)
-     
-     norm_val = mass_norm_val(L, beta, rho)
+    # Inertia for the directions of the modes - per unit length
+    inertia_vec = np.array([rho, rho, rotJ])
 
-     return unscaled_val / norm_val
+    # Frequencies for the three modes
+    omega = betaL**2 / L**2 * np.sqrt(stiffness_vec / inertia_vec)
 
+    # Stiffness Matrix
+    Kmat = np.diag(omega**2 / np.array([bending_phi, bending_phi, torsion_phi])**2)
 
-##################
-# Actual Calls to function etc
+    Mmat = np.diag(1.0 / np.array([bending_phi, bending_phi, torsion_phi])**2)
 
-# Beta * L for the first mode from pg 396 of Fundamentals of Structual Dynamics by Craig and Kurdila
-betaL = 1.8751 
+    # Modal Loading (uniform)
+    bending_f = unif_modal_loading(L, beta, rho)
+    torsion_f = unif_modal_loading(L, beta, rotJ)
 
-L = 1 # m
+    Ttrans = np.diag([bending_f / bending_phi, bending_f/bending_phi, torsion_f/torsion_phi])
 
-rho = 1.0 # density per unit length
+    return omega, Kmat, Mmat, Ttrans
 
-#### Calculated Parameters
+if __name__=="__main__":
 
-beta = betaL / L
+    ############
+    # Parameters of the model
+    
+    # Beta * L for the first mode from pg 396 of Fundamentals of Structual Dynamics by Craig and Kurdila
+    betaL = 1.8751 
+    
+    L = 2.0 # m
+    
 
-#### Check Mode Shape Normalization
+    span_frac = 0.7826179487179487
+   
+    density = 1.3 
 
-xplot = np.linspace(0, L, 200)
+    Emod = 1e6 # Elastic Modulus, Pa
+    poisson = 0.3
+    chord = 2.8 # chord length, m
+    width = 1 # width of rectangle, m
+    
+    #########
+    # Calculated Parameters
+    
+    beta = betaL / L
 
-phi_norm = mass_norm_modes(xplot, L, beta, rho)
+    span_pos = span_frac * L
+   
+    shearMod = Emod / 2.0 / (1 + poisson)
 
-integral = np.trapz(phi_norm**2 * rho, xplot)
+    # torsion calculation
+    a = chord / 2.0
+    b = width / 2.0
 
-print(integral)
+    Ktorsion = a*b**3*(16.0/3.0 - 3.36*b/a*(1 - b**4/12/a**4))
+ 
+    # Stiffness for flap, edge, torsion directions
+    # Bending directions should be E*I
+    # Torsion direction should be G*J
+    stiffness_vec = np.array([Emod*chord*width**3/12, Emod*chord**3*width/12, Ktorsion*shearMod])
 
-####
-# Plot the mode shape to check
+    rotJ = density*chord*width/12*(chord**2 + width**2) # rotational inertia per unit length for the torsion mode
+    rho = density*chord*width # density per unit length
 
-xplot = np.linspace(0, L, 200)
-# phi = unscaled_modes(xplot, L, beta)
-phi = mass_norm_modes(xplot, L, beta, rho)
+    # # For Debugging Inputs
+    # print('Stiffness Vec:')
+    # print(stiffness_vec)
 
-plt.plot(xplot, phi)
-plt.xlim((0, L))
+    # print('Inertia Vec:')
+    # print([rho, rho, rotJ])
 
-plt.savefig('FirstMode.png')
+    # print('[E, G]')
+    # print([Emod, shearMod])
 
+    #########
+    # Check Mode Shape Normalization 
+    xplot = np.linspace(0, L, 200) 
+    phi_norm = mass_norm_modes(xplot, L, beta, rho)
+    
+    integral = np.trapz(phi_norm**2 * rho, x=xplot)
+    print('Mass norm check of analytical modes (should be 1): {}'.format(integral))
+    
+    #########
+    # Plot the mode shape to check
+    xplot = np.linspace(0, L, 200)
+    phi = mass_norm_modes(xplot, L, beta, rho)
+    
+    plt.plot(xplot, phi)
+    plt.xlim((0, L)) 
+    plt.savefig('FirstMode.png')
+
+    #########
+    # Generate the system
+
+    omega, Kmat, Mmat, Ttrans = construct_analytical_model(L, beta, rho, rotJ, span_frac, stiffness_vec, betaL)
+
+    print('Frequencies: {} Hz'.format(str(omega.tolist())))
+
+    print('\nMass:')
+    print(Mmat)
+
+    print('\nStiffness:')
+    print(Kmat)
+
+    print('\nForce Transform Matrix:')
+    print(Ttrans)
